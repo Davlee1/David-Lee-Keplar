fd9a2b6d414733500fb7da98def22d3b167b07fd
Davlee1 <davidlee21019@gmail.com>
2025-05-25 20:29:21 -0400

    forgot to add html and js files

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const body = document.querySelector("body");
+const footerElement = document.createElement("footer");
+body.appendChild(footerElement);
+
+let today = new Date();
+let thisYear = today.getFullYear();
+
+let copyright = document.createElement('p')
+copyright.innerHTML = "<p class= 'copyright'>David Lee " + thisYear + ' \u{00A9}</p>'
+
+footerElement.appendChild(copyright);
+
+let skills = [ "HTML", "CSS", "JavaScript", "C#", "SQL", "PHP", "Verilog", "Haskell", "GitHub"];
+let skillsSection = document.querySelector(".skills-container");
+for (let i = 0; i < skills.length; i++){
+    let skill = document.createElement("li");
+    skill.innerHTML = '<li class="skills-block">'+ skills[i] +'</li>';
+    skillsSection.appendChild(skill);
+}
+
+
